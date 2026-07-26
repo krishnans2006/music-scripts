1a73806c09a44bfd7eceb79586d483e02d8f6bdf
Krishnan Shankar <krishnans2006@gmail.com>
2026-07-26 18:20:11 -0400

    Add initial beets script for processing

---
+++ 02process.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# This script uses beets (https://beets.io/) to:
+# - Fingerprint music using AcoustID
+# - Fetch metadata from MusicBrainz
+# - Tag music files with the fetched metadata
+# - Fetch lyrics
+# - Move the processed music and lyrics to a Jellyfin-compatible directory structure (./final)
+
+set -e
+
+if [ ! -d "./staging" ] || [ -z "$(ls -A ./staging)" ]; then
+   echo "No new music downloaded in ./staging. Exiting."
+   exit 0
+else
+   beet -c ./beets.yaml import -q --set playlist_source="Liked Music" ./staging
+fi
