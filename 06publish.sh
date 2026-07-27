a74a1036ae9081c0320f4c1c492dfa48cf50e17f
Krishnan Shankar <krishnans2006@gmail.com>
2026-07-27 16:38:00 -0400

    Fix 06publish permission issues

--- 06publish.sh
+++ 06publish.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-rsync -av --delete ./final/ krishnan@krishnan-pi:/data/jellyfin/media/krishnan-music/
+rsync -av -O --no-o --no-g --no-p --delete ./final/ krishnan@krishnan-pi:/data/jellyfin/media/krishnan-music/
+
+ssh krishnan@krishnan-pi "sudo chown -R jellyfin:jellyfin /data/jellyfin/media/krishnan-music/"
