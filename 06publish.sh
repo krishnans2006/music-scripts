022fa6a10648eb6ca6089d6baf2681631e674fbc
Krishnan Shankar <krishnans2006@gmail.com>
2026-07-28 17:16:58 -0400

    Fix permissions issue by using sudo with rsync

--- 06publish.sh
+++ 06publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-rsync -av -O --no-o --no-g --no-p --delete ./final/ krishnan@krishnan-pi:/data/jellyfin/media/krishnan-music/
+rsync -av --rsync-path="sudo rsync" --chown=jellyfin:jellyfin --delete ./final/ krishnan@krishnan-pi:/data/jellyfin/media/krishnan-music/
 
 ssh krishnan@krishnan-pi "sudo chown -R jellyfin:jellyfin /data/jellyfin/media/krishnan-music/"
